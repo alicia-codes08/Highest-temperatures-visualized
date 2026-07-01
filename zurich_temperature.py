6ea5e07f0382330a17f8849c9e7eebe6931dcde3
Alicia <Alicia@sigma.com>
2026-07-01 22:42:14 +0200

    Corrected extracted data

--- zurich_temperature.py
+++ zurich_temperature.py
@@ -16,8 +16,8 @@ for index, element in enumerate(header_row):
 # extract data
 temperatures, dates = [], []
 for row in reader:
-    date = datetime.strptime(row[0][:10], "%Y-%m-%d")
-    if row[4] == "°C":
+    if row[2]=="T_max_h1" and row[4]=="°C":
+        date = datetime.strptime(row[0][:10], "%Y-%m-%d")
         try:
             value = float(row[5].replace(",", "."))
         except ValueError:
@@ -30,8 +30,8 @@ for row in reader:
 # visualize extraced data
 plt.style.use('grayscale')
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.plot(dates, temperatures, color="blue", linewidth=0.8)
-ax.set_title("Temperature in Zurich (2025)", fontsize=20)
+ax.plot(dates, temperatures, color="red")
+ax.set_title("Max. temperatures in Zurich (2025)", fontsize=20)
 ax.set_ylabel("Temperature in °C", fontsize=16)
 ax.tick_params(axis="both", labelsize=13)
 
