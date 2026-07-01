cb3a5c5048d88156f18feb713ae6627b90d17444
Alicia <Alicia@sigma.com>
2026-07-01 22:25:32 +0200

    Extracted data ig?

--- zurich_temperature.py
+++ zurich_temperature.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt 
 from pathlib import Path 
+from datetime import datetime
 
 path = Path("weather_data/meteo_2025.csv")
 lines = path.read_text(encoding="utf-8").splitlines() 
@@ -15,12 +16,15 @@ for index, element in enumerate(header_row):
 # extract data
 temperatures, dates = [], []
 for row in reader:
+    date = datetime.strptime(row[0][:10], "%Y-%m-%d")
     if row[4] == "°C":
         try:
             value = float(row[5].replace(",", "."))
         except ValueError:
-            print("NA")
+            print(f"'NA' for {date}")
         else:
             temperatures.append(value)
+            dates.append(date)
 
 print(temperatures) 
+print(dates) 
